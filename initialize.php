e75a8065e63d13cfc7e3f5152ef9c0d37f561123
NombuleloShongwe <c3ntia@gmail.com>
2024-02-28 15:14:59 +0200

    Update initialize.php
    Updated Database credentials

--- initialize.php
+++ initialize.php
@@ -3,8 +3,8 @@
 if(!defined('base_url')) define('base_url','http://localhost/php-ocls/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_SERVER')) define('DB_SERVER',"team2.ch0ayyeuqxe8.us-east-1.rds.amazonaws.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"HomeFind.Team2");
 if(!defined('DB_NAME')) define('DB_NAME',"ocls_db");
 ?>
