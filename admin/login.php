c97071c817a73cbe6d933091efe8facdfcec6f28
SacramentoBaka <sacramentomakhubele@gmail.com>
2024-02-29 21:32:19 +0200

    Admin Login Design updated

--- admin/login.php
+++ admin/login.php
@@ -1,19 +1,139 @@
 <?php require_once('../config.php') ?>
 <!DOCTYPE html>
 <html lang="en" class="" style="height: auto;">
- <?php require_once('inc/header.php') ?>
+<?php require_once('inc/header.php') ?>
+
 <body class="hold-transition login-page">
   <script>
     start_loader()
   </script>
   <style>
-    body{
-      background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
-      background-size:cover;
-      background-repeat:no-repeat;
-      backdrop-filter: contrast(1);
+    * {
+      margin: 0;
+      padding: 0;
+      font-family: "Poppins", sans-serif;
+      box-sizing: border-box;
     }
-    #page-title{
+
+    body {
+      left: auto;
+      background-image: url("<?php echo validate_image($_settings->info('cover')) ?>");
+      background-size: cover;
+      background-repeat: no-repeat;
+      backdrop-filter: blur(20px);
+    }
+
+    .card {
+      width: 480px;
+      height: max-content;
+      position: relative;
+      border-radius: 25px;
+      margin: 0 -50px;
+    }
+
+    .card-body {
+      color: black;
+      border: 3px solid;
+      border-color: green;
+      border-radius: 25px;
+      background-size: cover;
+      background-position: center;
+      background: linear-gradient(95deg, #02fa44, #f5f5e9, #f5f5e9, #02fa44);
+      background-size: 300% 300%;
+      animation: color 24s ease-in-out infinite;
+    }
+
+    .card-body .login-box-msg {
+      font-size: 24px;
+      text-align: center;
+      font-weight: 700;
+      font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    @keyframes color {
+      0% {
+        background-position: 0 50%;
+      }
+
+      50% {
+        background-position: 100% 50%;
+      }
+
+      100% {
+        background-position: 0 50%;
+      }
+    }
+
+    .input-group {
+      width: 100%;
+      height: 40px;
+      border-bottom: 3px solid green;
+      border-bottom-left-radius: 15px;
+      border-bottom-right-radius: 15px;
+      margin: 35px 0;
+    }
+
+    .input-group input {
+      width: 100%;
+      height: 100%;
+      background: wheat;
+      border-radius: 15px;
+      border: none;
+      outline: none;
+      font-size: 16px;
+      color: black;
+      padding-left: 10px;
+      font-weight: 500;
+      padding-right: 28px;
+    }
+
+    .input-group label {
+      position: absolute;
+      top: auto;
+      left: 10px;
+      transform: translateY(-100%);
+      font-size: 16px;
+      font-weight: 500;
+      pointer-events: none;
+    }
+
+    .row button {
+      width: 100%;
+      margin: 0 10px;
+      height: 35px;
+      margin-top: 15px;
+      font-weight: 700;
+      background-color: wheat;
+      border: 2px solid green;
+      cursor: pointer;
+      font-size: 14px;
+      border-radius: 15px;
+      color: black;
+      box-shadow: 0 0 10px rgba(241, 238, 238, 0.5);
+      transition: .5s ease;
+    }
+
+    .goto-client {
+      font-size: 14.5px;
+      font-weight: 600;
+      text-align: center;
+      margin-top: 25px;
+    }
+
+    .goto-client p a {
+      text-align: center;
+      font-weight: 700;
+      font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
+
+    .goto-client p a:hover {
+      text-decoration: underline;
+      font-weight: 700;
+      font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+      color: green;
+    }
+
+    #page-title {
       text-shadow: 6px 4px 7px black;
       font-size: 3.5em;
       color: #fff4f4 !important;
@@ -21,63 +141,58 @@
     }
   </style>
   <h1 class="text-center text-white px-4 py-5" id="page-title"><b><?php echo $_settings->info('name') ?></b></h1>
-<div class="login-box">
-  <!-- /.login-logo -->
-  <div class="card card-primary my-2">
-    <div class="card-body">
-      <p class="login-box-msg"> Enter your credentials</p>
-      <form id="login-frm" action="" method="post">
-        <div class="input-group mb-3">
-          <input type="text" class="form-control" name="username" autofocus placeholder="Username">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-user"></span>
-            </div>
+  <div class="login-box">
+    <!-- /.login-logo -->
+    <!-- before <div class="card card-primary my-2">-->
+    <div class="card">
+      <div class="card-body">
+        <p class="login-box-msg"> Enter Admin Credentials</p>
+        <form id="login-frm" action="" method="post">
+          <div class="input-group">
+            <input type="text" name="username">
+            <label>Email</label>
           </div>
-        </div>
-        <div class="input-group mb-3">
-          <input type="password" class="form-control"  name="password" placeholder="Password">
-          <div class="input-group-append">
-            <div class="input-group-text">
-              <span class="fas fa-lock"></span>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-8">
-            <a href="<?php echo base_url ?>">Go to Website</a>
-          </div>
-          <!-- /.col -->
-          <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-          </div>
-          <!-- /.col -->
-        </div>
-      </form>
-      <!-- /.social-auth-links -->
+          <div class="input-group">
+            <input type="password" name="password">
+            <label>Password</label>
 
-      <!-- <p class="mb-1">
+          </div>
+          <div class="row">
+            <button type="submit">Sign In</button>
+          </div>
+          <div class="goto-client">
+            <p>
+              Go back to
+              <a href="<?php echo base_url ?>">Website</a>
+            </p>
+          </div>
+
+        </form>
+        <!-- /.social-auth-links -->
+
+        <!-- <p class="mb-1">
         <a href="forgot-password.html">I forgot my password</a>
       </p> -->
-      
+
+      </div>
+      <!-- /.card-body -->
     </div>
-    <!-- /.card-body -->
+    <!-- /.card -->
   </div>
-  <!-- /.card -->
-</div>
-<!-- /.login-box -->
+  <!-- /.login-box -->
 
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.min.js"></script>
 
-<script>
-  $(document).ready(function(){
-    end_loader();
-  })
-</script>
+  <script>
+    $(document).ready(function() {
+      end_loader();
+    })
+  </script>
 </body>
+
 </html>
