a9796b42e617e9f372ad7e50dbfce7a47acffc5d
AthuleVusani <athulevusani@gmail.com>
2024-03-07 01:41:26 +0200

    Edited About Us

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="card rounded-0">
             <div class="card-body">
-                <h2>About Us</h2>
+                <h2>About PC Paradise</h2>
                 <p>Welcome to PC Paradise, your ultimate destination for all things computing! Founded by a group of passionate computer enthusiasts, PC Paradise aims to provide top-notch products and exceptional service to tech-savvy customers like you.</p>
                 <p>At PC Paradise, we understand the importance of staying ahead in today's rapidly evolving digital landscape. Whether you're a hardcore gamer, a professional designer, or a casual user, we've got you covered with a wide range of products tailored to suit your needs.</p>
                 <p>Our mission is simple: to empower you with the latest technology, delivered with unparalleled customer satisfaction. From cutting-edge gaming rigs and high-performance laptops to reliable desktops and essential accessories, we handpick each product to ensure quality, reliability, and value.</p>
