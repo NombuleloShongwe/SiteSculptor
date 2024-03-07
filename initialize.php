b62e85e0f1410dd80d1a1c9851c1ab960d105e7c
SacramentoBaka <sacramentomakhubele@gmail.com>
2024-03-07 09:42:52 +0200

    DB Connection Updated
    Changes on the DB credentials

--- initialize.php
+++ initialize.php
@@ -1,10 +1,9 @@
 <?php
 // $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/SiteSculptor/');
-if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
+if (!defined('base_url')) define('base_url', 'http://localhost/SiteSculptor/');
+if (!defined('base_app')) define('base_app', str_replace('\\', '/', __DIR__) . '/');
 // if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"team2.ch0ayyeuqxe8.us-east-1.rds.amazonaws.com");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"HomeFind.Team2");
-if(!defined('DB_NAME')) define('DB_NAME',"ocls_db");
-?>
+if (!defined('DB_SERVER')) define('DB_SERVER', "sql.freedb.tech");
+if (!defined('DB_USERNAME')) define('DB_USERNAME', "freedb_Team02");
+if (!defined('DB_PASSWORD')) define('DB_PASSWORD', "?mFvXAFH#EA5tb6");
+if (!defined('DB_NAME')) define('DB_NAME', "freedb_SculptorDB");
