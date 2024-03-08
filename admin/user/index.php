88d94b954135549ed11e1b89b2523f5d11ecb6d4
NikiweMbhiza <126746700+NikiweMbhiza@users.noreply.github.com>
2024-03-08 10:51:52 +0200

    Update index.php

--- admin/user/index.php
+++ admin/user/index.php
@@ -13,7 +13,7 @@ foreach($user->fetch_array() as $k =>$v){
 	<div class="card-body">
 		<div class="container-fluid">
 			<div id="msg"></div>
-			<form action="" id="manage-user"style="background-color: rgb(175, 195, 168);">	
+			<form action="" id="manage-user">	
 				<input type="hidden" name="id" value="<?php echo $_settings->userdata('id') ?>">
 				<div class="form-group">
 					<label for="name"style="color: green;">First Name(s)</label>
